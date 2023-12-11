855c3d15969d0bc7298850a0b0e2fd2e0dbbd275
Mohamed Elassy <103861551+elassyex@users.noreply.github.com>
2023-12-12 00:19:54 +0300

    Update urls.py

--- translator/urls.py
+++ translator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import translate_text
+from .views import translate_text,home
 
 urlpatterns = [
     path('translate/', translate_text, name='translate_text'),
+    path('', home ,name='home')
 ]
